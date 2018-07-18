f28b33cdfc35168668900880cfca0a1d4f0e03c2
Dai Luyao <DaiLuyaoME@foxmail.com>
2018-07-18 17:09:35 +0800

    revise filterTest.m to enable use of transfer function, not just digitalFilter object

--- filterTest.m
+++ filterTest.m
@@ -4,3 +4,19 @@ dataFilter3 = designfilt('lowpassiir', 'FilterOrder', 4, 'PassbandFrequency', .0
 % dataFilter4 = designfilt('lowpassiir', 'FilterOrder', 4, 'PassbandFrequency', .005, 'PassbandRipple', 0.01);
 % fvtool(dataFilter1,dataFilter2,dataFilter3,dataFilter4);
 fvtool(dataFilter1,dataFilter2,dataFilter3);
+%%
+% fs = 49;
+% cutoffFrequency = 0.003;
+cutoffFrequency = 0.120;
+fs = 5000;
+dataFilter1 = designfilt('lowpassiir', 'FilterOrder', 3, 'PassbandFrequency', cutoffFrequency, 'PassbandRipple', 0.01);
+[b1,a1] = tf(dataFilter1);
+[b2,a2] = secOrderFilter(cutoffFrequency/2 * fs, fs);
+filterTemp1 = tf(b1,a1,1/fs);
+filterTemp2 = tf(b2,a2,1/fs);
+opts = bodeoptions;
+opts.FreqUnits = 'Hz';
+opts.PhaseMatching = 'on';
+opts.PhaseMatchingFreq = 0.01;
+opts.PhaseMatchingValue = 0;
+figure;bodeplot(filterTemp1,filterTemp2,opts);
